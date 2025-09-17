ba8c2d421bc4219e50a1a4e52741fd8661e2a0aa
jgoode7 <joegoode.07@gmail.com>
2025-09-17 11:31:03 -0700

    Add subtract case using add template

--- JavaCalculator.java
+++ JavaCalculator.java
@@ -16,6 +16,14 @@ public class JavaCalculator {
                 int addTotal = add1 + add2;
                 System.out.println("The result is: " + addTotal);
                 break;
+
+            case "-":
+                System.out.print("Enter the two numbers you'd like subtracted:");
+                int sub1 = scnr.nextInt();
+                int sub2 = scnr.nextInt();
+                int subTotal = sub1 + sub2;
+                System.out.println("The result is: " + subTotal);
+                break;
         }
     }
 }
