2fbe29f95dd107abc289b04adec8a60f23e2b60b
jgoode7 <joegoode.07@gmail.com>
2025-09-17 11:29:31 -0700

    Fix scanner issue. add case for addition
    Tested and working

--- JavaCalculator.java
+++ JavaCalculator.java
@@ -2,10 +2,20 @@ import java.util.Scanner;
 
 public class JavaCalculator {
     public static void main(String[] args) {
-        Scanner scnr = new Scanner(Scanner.in);
+        Scanner scnr = new Scanner(System.in);
 
         System.out.println("Welcome to the Java Calculator Program.");
         System.out.println("Which operation would you like to perform? (+, -, *. pow, sqrt)");
         String operatorType = scnr.nextLine();
+
+        switch (operatorType) {
+            case "+":
+                System.out.print("Enter the two numbers you'd like added:");
+                int add1 = scnr.nextInt();
+                int add2 = scnr.nextInt();
+                int addTotal = add1 + add2;
+                System.out.println("The result is: " + addTotal);
+                break;
+        }
     }
 }
