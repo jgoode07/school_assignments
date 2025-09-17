9ac2331fa448aefef3a8d20e9320ee4c9c53fdce
jgoode7 <joegoode.07@gmail.com>
2025-09-17 11:06:40 -0700

    Print welcome statements

---
+++ JavaCalculator.java
@@ -0,0 +1,6 @@
+public class JavaCalculator {
+    public static void main(String[] args) {
+        System.out.println("Welcome to the Java Calculator Program.")
+        System.out.println("Which operation would you like to perform?")
+    }
+}``
