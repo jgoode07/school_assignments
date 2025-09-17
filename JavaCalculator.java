6f4ee0b297439fccf98ba2c4f97ea2f4dbe6f49e
jgoode7 <joegoode.07@gmail.com>
2025-09-17 11:57:11 -0700

    Create while statement
    Place switch statement inside of while

--- JavaCalculator.java
+++ JavaCalculator.java
@@ -8,53 +8,58 @@ public class JavaCalculator {
         System.out.print("Which operation would you like to perform? (+, -, *. pow, sqrt): ");
         String operatorType = scnr.nextLine();
 
-        switch (operatorType) {
-            case "+":
-                System.out.print("Enter the two numbers you'd like added: ");
-                int add1 = scnr.nextInt();
-                int add2 = scnr.nextInt();
-                int addTotal = add1 + add2;
-                System.out.println("The result is: " + addTotal);
-                break;
+        boolean anotherOne = true;
 
-            case "-":
-                System.out.print("Enter the two numbers you'd like subtracted: ");
-                int sub1 = scnr.nextInt();
-                int sub2 = scnr.nextInt();
-                int subTotal = sub1 - sub2;
-                System.out.println("The result is: " + subTotal);
-                break;
+        while (anotherOne) {
 
-            case "*":
-                System.out.print("Enter the two numbers you'd like multiplied: ");
-                int multi1 = scnr.nextInt();
-                int multi2 = scnr.nextInt();
-                int multiTotal = multi1 * multi2;
-                System.out.println("The result is: " + multiTotal);
-                break;
+            switch (operatorType) {
+                case "+":
+                    System.out.print("Enter the two numbers you'd like added: ");
+                    int add1 = scnr.nextInt();
+                    int add2 = scnr.nextInt();
+                    int addTotal = add1 + add2;
+                    System.out.println("The result is: " + addTotal);
+                    break;
 
-            case "pow":
-                System.out.print("Enter the base number: ");
-                int base = scnr.nextInt();
+                case "-":
+                    System.out.print("Enter the two numbers you'd like subtracted: ");
+                    int sub1 = scnr.nextInt();
+                    int sub2 = scnr.nextInt();
+                    int subTotal = sub1 - sub2;
+                    System.out.println("The result is: " + subTotal);
+                    break;
 
-                System.out.print("Enter the exponent you want to raise it to: ");
-                int exponent = scnr.nextInt();
+                case "*":
+                    System.out.print("Enter the two numbers you'd like multiplied: ");
+                    int multi1 = scnr.nextInt();
+                    int multi2 = scnr.nextInt();
+                    int multiTotal = multi1 * multi2;
+                    System.out.println("The result is: " + multiTotal);
+                    break;
 
-                double powTotal = Math.pow(base, exponent);
-                System.out.println("The result is: " + powTotal);
-                break;
+                case "pow":
+                    System.out.print("Enter the base number: ");
+                    int base = scnr.nextInt();
 
-            case "sqrt":
-                System.out.print("Enter the number you want the square root of: ");
-                int sqrtNum = scnr.nextInt();
+                    System.out.print("Enter the exponent you want to raise it to: ");
+                    int exponent = scnr.nextInt();
 
-                double sqrtTotal = Math.sqrt(sqrtNum);
-                System.out.println("The result is: " + sqrtTotal);
-                break;
+                    double powTotal = Math.pow(base, exponent);
+                    System.out.println("The result is: " + powTotal);
+                    break;
 
-            default:
-                System.out.println("Invalid option, please enter a valid operator.");
-                break;
+                case "sqrt":
+                    System.out.print("Enter the number you want the square root of: ");
+                    int sqrtNum = scnr.nextInt();
+
+                    double sqrtTotal = Math.sqrt(sqrtNum);
+                    System.out.println("The result is: " + sqrtTotal);
+                    break;
+
+                default:
+                    System.out.println("Invalid option, please enter a valid operator.");
+                    break;
+            }
         }
     }
 }
