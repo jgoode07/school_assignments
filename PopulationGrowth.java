a9fd8f922ac20f928ae2959d4c8c5bf92b9ae404
jgoode7 <joegoode.07@gmail.com>
2025-09-18 11:54:04 -0700

    Add validation 'if' statement
    Change startingNum to double

--- PopulationGrowth.java
+++ PopulationGrowth.java
@@ -5,11 +5,20 @@ public class PopulationGrowth {
         Scanner scnr = new Scanner(System.in);
 
         System.out.println("Welcome to the Population Growth program.");
-        System.out.print("Please enter the starting number of organisms (2 or more): ");
-        int startingNum = scnr.nextInt();
 
-        System.out.println("What is the daily increase of population in percentage (positive only): ");
+        System.out.print("Please enter the starting number of organisms (2 or more): ");
+        double startingNum = scnr.nextDouble();
+
+        System.out.print("What is the daily increase of population in percentage (positive only): ");
         int dailyGrowth = scnr.nextInt();
+
+        System.out.print("How many days will the population multiply for? ");
+        int numOfDays = scnr.nextInt();
+
+        if (startingNum < 2 || dailyGrowth < 0 || numOfDays < 1) {
+            System.out.println("Invalid input. Please follow the instructions properly.");
+            return;
+        }
 
     }
 }
