8633da88e8a9f5298c32dadd322180ef464b24e9
jgoode7 <joegoode.07@gmail.com>
2025-09-18 11:33:27 -0700

    Create class, main
    Import scanner,
    Create welcoming messages

---
+++ PopulationGrowth.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class PopulationGrowth {
+    public static void main(String[] args) {
+        Scanner scnr = new Scanner(System.in);
+
+        System.out.println("Welcome to the Population Growth program.");
+        System.out.println("Please enter the starting number of organisms (2 or more): ");
+
+    }
+}
