0b39c7c9aafa7ebc2d29161e40f23ff847ecd7f4
jgoode7 <joegoode.07@gmail.com>
2025-09-18 12:32:57 -0700

    Create for loop to iterate through days
    -Program runs properly as-is
    -Fatal program inputs are not ideal, considering a while loop to continue after invalid input

--- PopulationGrowth.java
+++ PopulationGrowth.java
@@ -26,5 +26,12 @@ public class PopulationGrowth {
             System.out.println("Invalid input. Number of days must be 1 or more.");
             return;
         }
+
+        System.out.println("\nPopulation growth over " + numOfDays + " days:");
+        double population = startingNum;
+        for (int i = 1; i <= numOfDays; i++) {
+            System.out.println("Day " + i + ": " + String.format("%.2f", population) + " organisms");
+            population += population * dailyGrowth / 100.0;
+        }
     }
 }
