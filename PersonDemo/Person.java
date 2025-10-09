3bb995221adb375e35088854549639496e17fe57
jgoode7 <joegoode.07@gmail.com>
2025-10-09 13:20:31 -0700

    Add prologues, private variables

--- PersonDemo/Person.java
+++ PersonDemo/Person.java
@@ -1,5 +1,15 @@
+/********************************
+* Person.java
+* Author: Joe Goode
+* 
+* Create a simple Person class
+********************************/
+
 package PersonDemo;
 
 public class Person {
-
+    // Instance variables for the person's info
+    private String name;
+    private int age;
+    private String email;
 }
--- PersonDemo/PersonDemo.java
+++ PersonDemo/PersonDemo.java
@@ -1,3 +1,10 @@
+/******************************
+* PersonDemo.java
+* Author: Joe Goode
+* 
+* Testing the Person class
+*******************************/
+
 package PersonDemo;
 
 public class PersonDemo {
