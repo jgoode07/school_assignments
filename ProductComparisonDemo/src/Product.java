3a1950f57d1a3146ffd78ed80ff5e561be881cd9
jgoode7 <joegoode.07@gmail.com>
2025-10-17 11:48:02 -0700

    Add constructor to initialize the attributes

--- ProductComparisonDemo/src/Product.java
+++ ProductComparisonDemo/src/Product.java
@@ -2,4 +2,10 @@ public class Product {
     private String name;
     private double price;
     private int productCode;
+
+    public Product(String name, double price, int productCode) {
+        this.name = name;
+        this.price = price;
+        this.productCode = productCode;
+    }
 }
