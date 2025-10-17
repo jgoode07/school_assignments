d4014ab35403d9c39a603a4cb2a63c01e042bd67
jgoode7 <joegoode.07@gmail.com>
2025-10-16 18:58:06 -0700

    Create 1 param constructor, one setter
    Setter to satisfy the compiler

--- DisplayWeight/src/Weight.java
+++ DisplayWeight/src/Weight.java
@@ -2,10 +2,19 @@ public class Weight {
     private double weight;
     private String units;
 
-    public setWeight()
-
+    // Set a default zero parameter constructor
     public Weight() {
         weight = 0;
+        units = "kg";
+    }
+
+    // One param constructor
+    public Weight(double w) {
+        setWeight(w);
+    }
+
+    public void setWeight(double w) {
+        weight = w;
         units = "kg";
     }
 }
