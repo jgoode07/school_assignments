ee6f20ce26acd721ba55884e6d28b1a0124647c2
jgoode7 <joegoode.07@gmail.com>
2025-10-17 17:30:58 -0700

    Create boolean overloaded constructor

--- ProductComparisonDemo/src/Product.java
+++ ProductComparisonDemo/src/Product.java
@@ -13,4 +13,16 @@ public class Product {
         System.out.println("Name: " + name);
         System.out.println("Price: $" + price);
     }
+
+    // Overloaded version that takes a boolean parameter
+    // The parameter (showCode) decides whether to include the product code
+    public void printProductInfo(boolean showCode) {
+        if (showCode) {
+            System.out.println("Name: " + name);
+            System.out.println("Price: $" + price);
+            System.out.println("Product Code: " + productCode);
+        } else {
+            printProductInfo(); // Calls the first version w/o product code
+        }
+    }
 }
