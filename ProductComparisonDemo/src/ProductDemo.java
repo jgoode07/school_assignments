a80ed04b41b924ae01bb0093b70a451066c9ec49
jgoode7 <joegoode.07@gmail.com>
2025-10-17 18:40:05 -0700

    Add both overloaded methods
    Prints as expected

--- ProductComparisonDemo/src/ProductDemo.java
+++ ProductComparisonDemo/src/ProductDemo.java
@@ -7,5 +7,13 @@ public class ProductDemo {
         System.out.println("Both products are equal: " + product1.equals(product2)); // Prints "false"
 
         Product product3 = new Product("Tablet", 499.99, 12346);
+
+        // Use both versions of the overloaded methods
+        System.out.println("Product 3 Info: ");
+        product3.printProductInfo(false); // Prints Name and Price only
+
+        System.out.println("Product 3 Info:");
+        product3.printProductInfo(true); // Prints Name, Price and Code
     }
+
 }
