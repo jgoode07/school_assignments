c46d14f927598e6b0f95709ea59ba2b711617323
jgoode7 <joegoode.07@gmail.com>
2025-10-17 17:33:01 -0700

    Clarify equals method print statement

--- ProductComparisonDemo/src/ProductDemo.java
+++ ProductComparisonDemo/src/ProductDemo.java
@@ -3,6 +3,6 @@ public class ProductDemo {
         Product product1 = new Product("Laptop", 999.99, 12345);
         // System.out.println("Product created successfully!");
         Product product2 = new Product("Laptop", 999.99, 12345);
-        System.out.println(product1.equals(product2)); // Prints "false"
+        System.out.println("Both products are equal: " + product1.equals(product2)); // Prints "false"
     }
 }
