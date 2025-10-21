2b90fa6c6b6bf4564974e602c2ab7bcb7f5e9e95
jgoode7 <joegoode.07@gmail.com>
2025-10-21 13:16:15 -0700

    Start with Code provided

---
+++ Cousins/src/Cousins.java
@@ -0,0 +1,16 @@
+import java.util.Arrays;
+
+public class Cousins {
+
+    public static void main(String[] args) {
+        String[] bradKids = { "Maddy", "Ross", "Henry" };
+        String[] jayKids = { "Brian", "Kevin", "Kristin" };
+        String[] annKids = {};
+        String[] libbyKids = { "Ellie", "Hanna", "Jack", "Ben" };
+        String[][] cousins = { bradKids, jayKids, annKids, libbyKids };
+
+        // <insert code here>
+
+    } // end Main
+
+} // end class Cousins
