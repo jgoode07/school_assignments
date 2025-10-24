433d0cdd714dd193e8eef0ccdc883e0797507633
jgoode7 <joegoode.07@gmail.com>
2025-10-23 18:46:29 -0700

    Create files, add private variables, constructor
    To Product.java

---
+++ ProductManager/src/Product.java
@@ -0,0 +1,11 @@
+public class Product {
+    private String productName;
+    private int productId;
+    private double[] prices;
+
+    public Product(String productName, int productId, double[] prices) {
+        this.productName = productName;
+        this.productId = productId;
+        this.prices = prices;
+    }
+}
