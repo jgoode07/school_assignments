fae0b9284d55897867846bfec04f33ef5d1e54a9
jgoode7 <joegoode.07@gmail.com>
2025-11-06 12:22:34 -0800

    Create two account objects

--- savingsAccount/src/SavingsAccountDriver.java
+++ savingsAccount/src/SavingsAccountDriver.java
@@ -1,3 +1,8 @@
 public class SavingsAccountDriver {
+    public static void main(String[] args) {
 
+        // Create the two objects/accounts
+        SavingsAccount saver1 = new SavingsAccount(10002, 2000.00);
+        SavingsAccount saver2 = new SavingsAccount(10003, 3000.00);
+    }
 }
