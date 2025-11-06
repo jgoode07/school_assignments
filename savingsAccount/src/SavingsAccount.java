d1225601018459efa68d8c0c1e9cbad9171b0db8
jgoode7 <joegoode.07@gmail.com>
2025-11-06 12:11:00 -0800

    Add constructor, accessors

--- savingsAccount/src/SavingsAccount.java
+++ savingsAccount/src/SavingsAccount.java
@@ -2,4 +2,19 @@ public class SavingsAccount {
     private static double annualInterestRate;
     private final int ACCOUNT_NUMBER;
     private double balance;
+
+    // Constructor
+    public SavingsAccount(int accountNumber, double initialBalance) {
+        this.ACCOUNT_NUMBER = accountNumber;
+        this.balance = initialBalance;
+    }
+
+    // Accessor Methods
+    public int getAccountNumber() {
+        return ACCOUNT_NUMBER;
+    }
+
+    public double getBalance() {
+        return balance;
+    }
 }
