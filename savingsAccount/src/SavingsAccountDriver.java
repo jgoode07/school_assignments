109565f1d209740adbfeed4749837eab492e28e6
jgoode7 <joegoode.07@gmail.com>
2025-11-06 12:33:25 -0800

    Add headings for the table

--- savingsAccount/src/SavingsAccountDriver.java
+++ savingsAccount/src/SavingsAccountDriver.java
@@ -4,5 +4,14 @@ public class SavingsAccountDriver {
         // Create the two objects/accounts
         SavingsAccount saver1 = new SavingsAccount(10002, 2000.00);
         SavingsAccount saver2 = new SavingsAccount(10003, 3000.00);
+
+        // Set annual interest rate
+        SavingsAccount.setAnnualInterestRate(0.05);
+
+        // Print table heading using \t for better spacing
+        System.out.println("Monthly balances for one year with 0.05 annual interest:\n");
+        System.out.println("Month\tAccount #\tBalance\t\tAccount #\tBalance");
+        System.out.println("-----\t---------\t-------\t\t---------\t-------");
+
     }
 }
