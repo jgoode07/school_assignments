43a6e142bf056c815a7433aaffc62f184df34a1f
jgoode7 <joegoode.07@gmail.com>
2025-11-07 09:22:19 -0800

    Add variable, constructor, method to Car class

--- CarProgram/src/Car.java
+++ CarProgram/src/Car.java
@@ -1,3 +1,14 @@
 public class Car {
+    // Instance variable
+    private double price;
 
+    // 1-param Constructor
+    public Car(double cost) {
+        price = cost * 2;
+    }
+
+    // Accessor method
+    public double getPrice() {
+        return price;
+    }
 }
