ae4d207c4e73a1720321393fb12714590d94124e
jgoode7 <joegoode.07@gmail.com>
2025-11-12 15:13:48 -0800

    Add load() method to Employee class

--- Payroll/src/Employee.java
+++ Payroll/src/Employee.java
@@ -1,7 +1,26 @@
+import java.util.Scanner;
+
 public class Employee {
     // Instance variables
     protected String name;
     protected String socialSecurityNumber;
     protected int birthdayMonth;
     protected int birthdayWeek;
+
+    public void load() {
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Name ==> ");
+        name = input.nextLine();
+
+        System.out.print("Social Security Number ==> ");
+        socialSecurityNumber = input.nextLine();
+
+        System.out.print("Birthday Month (1-12) ==> ");
+        birthdayMonth = input.nextInt();
+
+        System.out.print("Birthday Bonus Week (1-4) ==> ");
+        birthdayWeek = input.nextInt();
+    }
+
 }
