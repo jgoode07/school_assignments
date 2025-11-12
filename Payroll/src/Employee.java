a7c559c54719f770e36d462d0e3caf7d37776fb6
jgoode7 <joegoode.07@gmail.com>
2025-11-12 12:04:20 -0800

    Create Payroll project, add Employee superclass

---
+++ Payroll/src/Employee.java
@@ -0,0 +1,7 @@
+public class Employee {
+    // Instance variables
+    protected String name;
+    protected String socialSecurityNumber;
+    protected int birthdayMonth;
+    protected int birthdayWeek;
+}
