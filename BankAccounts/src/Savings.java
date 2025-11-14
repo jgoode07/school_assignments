77fe596b1b47bb552cb5257c627cae1c3af764a5
jgoode7 <joegoode.07@gmail.com>
2025-11-14 15:57:57 -0800

    Create Savings class, display using NumberFormat

---
+++ BankAccounts/src/Savings.java
@@ -0,0 +1,29 @@
+import java.text.NumberFormat;
+
+public class Savings extends BankAccount {
+
+    // Interest rate
+    private double intRate;
+
+    // Constructor
+    public Savings(double balance, double intRate) {
+        super(balance);
+        this.intRate = intRate;
+    }
+
+    public void addInterest() {
+        double interest = balance * intRate;
+        balance += interest;
+
+        if (balance < 0) {
+            balance = 0;
+        }
+    }
+
+    // Display method using NumberFormat
+    @Override
+    public void display() {
+        NumberFormat currency = NumberFormat.getCurrencyInstance();
+        System.out.println("Savings account balance = " + currency.format(balance));
+    }
+}
