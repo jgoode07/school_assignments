1cf986c563167e0a60e62355e96669f2899003af
jgoode7 <joegoode.07@gmail.com>
2025-11-13 18:28:08 -0800

    Create Salaried+Commission subclass

---
+++ Payroll/src/SalariedPlusCommission.java
@@ -0,0 +1,37 @@
+import java.util.Scanner;
+
+public class SalariedPlusCommission extends Salaried {
+
+    // Instance variables
+    private double sales;
+    private double commissionRate;
+
+    @Override
+    public void load() {
+        Scanner input = new Scanner(System.in);
+
+        // Call parent class load() first
+        super.load();
+
+        System.out.print("Sales for this past week ==> ");
+        sales = input.nextDouble();
+
+        System.out.print("Sales commission rate (fraction paid to employee) ==> ");
+        commissionRate = input.nextDouble();
+    }
+
+    @Override
+    public double getEarnings() {
+        double earnings = (sales * commissionRate) + weeklySalary;
+
+        // Cap paycheck at $1000
+        if (earnings > 1000) {
+            earnings = 1000;
+        }
+
+        // Add birthday bonus if applicable
+        getBonus(earnings);
+
+        return earnings;
+    }
+}
