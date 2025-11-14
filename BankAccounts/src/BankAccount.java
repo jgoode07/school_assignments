2b9835caa42a27f507715ad188410946ab44b2c5
jgoode7 <joegoode.07@gmail.com>
2025-11-14 15:11:48 -0800

    Create abstract BankAccount base class

--- BankAccounts/src/BankAccount.java
+++ BankAccounts/src/BankAccount.java
@@ -1,3 +1,25 @@
-public class BankAccount {
+public abstract class BankAccount {
 
+    // Instance variable
+    protected double balance;
+
+    // Deposit money into the account
+    public void deposit(double amount) {
+        if (amount > 0) {
+            balance += amount;
+        }
+    }
+
+    // Withdraw money (cannot go below 0)
+    public void withdraw(double amount) {
+        if (amount > 0) {
+            balance -= amount;
+            if (balance < 0) {
+                balance = 0;
+            }
+        }
+    }
+
+    // Abstract display method (to be defined in subclasses)
+    public abstract void display();
 }
